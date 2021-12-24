7eb1ba58e16e279288799ec46dd0660d6c34e8b5
akbarjon2000 <1543431a@gmail.com>
2021-12-24 22:58:06 +0900

    created navbar and body

---
+++ src/Components/header/index.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { Container } from "./style"
+const Header = () => {
+    const [active, setActive] = useState(false)
+    const onToggle = () => {
+        setActive(true);
+    }
+    const onClose = () => {
+        setActive(false)
+    }
+    const linkAction = () => {
+        setActive(false);
+    }
+    return (
+        <Container className='header' id='header' active={active}>
+            <nav className='nav container'>
+                <a href='#' className='nav__logo'>Akbarjon</a>
+                <div className='nav__menu' id='nav-menu' >
+                    <ul className='nav__list grid'>
+                        <li className='nav__item'>
+                            <a href='#home' className='nav__link' onClick={linkAction}>
+                                <i class="nav__icon uil uil-estate"></i> Home
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#user' className='nav__link' onClick={linkAction}>
+                                <i class="nav__icon uil uil-user"></i> User
+                            </a>
+                        </li><li className='nav__item'>
+                            <a href='#skills' className='nav__link' onClick={linkAction}>
+                                <i class="nav__icon uil uil-file-alt"></i> Skills
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#service' className='nav__link' onClick={linkAction}>
+                                <i class="nav__icon uil uil-briefcase-alt"></i> service
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#portfolio' className='nav__link' onClick={linkAction}>
+                                <i class="nav__icon uil uil-scenery"></i> Portfolio
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#contact' className='nav__link' onClick={linkAction}>
+                                <i class="nav__icon uil uil-message"></i> Contact me
+                            </a>
+                        </li>
+
+                    </ul>
+                    <i class="uil uil-times nav__close" id='nav-close' onClick={onClose}></i>
+                </div>
+                <div className='nav__btns '>
+                    <div onClick={onToggle} className='nav__toggle' id='nav-toggle'>
+                        <i class="uil uil-apps"></i>
+                    </div>
+                </div>
+            </nav>
+        </Container>
+    )
+}
+
+export default Header
